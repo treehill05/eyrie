fb17ae641c291842e41bb4edf17c195e43556e69
--global <--global>
2025-10-04 09:28:20 -0400

    remove unused deps

--- frontend/app/_components/drone-form.tsx
+++ frontend/app/_components/drone-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
